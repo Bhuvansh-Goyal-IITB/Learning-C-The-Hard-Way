cb2257d6be35e331e1f3601bb8500695981433b6
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-07-22 22:29:03 +0530

    Changed formatting

--- ex15/main.c
+++ ex15/main.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-  int ages[] = { 20, 17, 12, 10 };
-  char* names[] = {
-    "Krishna",
-    "Pankaj",
-    "Anupam",
-    "Vihaan"
-  };
+int main(int argc, char *argv[]) {
+  int ages[] = {20, 17, 12, 10};
+  char *names[] = {"Krishna", "Pankaj", "Anupam", "Vihaan"};
 
   int count = sizeof(ages) / sizeof(int);
   int i = 0;
@@ -17,10 +12,10 @@ int main(int argc, char* argv[]) {
   }
 
   printf("---\n");
-  
-  int* cur_age = ages;
-  char** cur_name = names;
-  
+
+  int *cur_age = ages;
+  char **cur_name = names;
+
   for (i = 0; i < count; i++) {
     printf("%s is %d years old\n", *(cur_name + i), *(cur_age + i));
   }
@@ -32,8 +27,9 @@ int main(int argc, char* argv[]) {
   }
 
   printf("---\n");
-  
-  for (cur_age = ages, cur_name = names; (cur_age - ages) < count; cur_name++, cur_age++) {
+
+  for (cur_age = ages, cur_name = names; (cur_age - ages) < count;
+       cur_name++, cur_age++) {
     printf("%s is %d years old\n", *cur_name, *cur_age);
   }
 
