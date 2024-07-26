d506df825e43c030223d5c7de985ec87a0826e5d
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-07-26 14:06:56 +0530

    Changed ex19 dbg.h formatting

--- ex19/dbg.h
+++ ex19/dbg.h
@@ -10,25 +10,24 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...)                                                 \
-  fprintf(stderr, "[DEBUG] (f: %s, l: %d, fn: %s) " M "\n", __FILE__, \
-          __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define debug(M, ...)                                                      \
+  fprintf(stderr, "[DEBUG] (f:%s l:%d fn:%s) " M "\n", __FILE__, __LINE__, \
+          __FUNCTION__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...)                                                        \
-  fprintf(stderr, BRIGHT_RED("[ERROR] (f: %s, l: %d, fn: %s, e: %s) " M "\n"), \
+#define log_err(M, ...)                                                 \
+  fprintf(stderr, BRIGHT_RED("[ERROR] (f:%s l:%d fn:%s e:%s) " M "\n"), \
           __FILE__, __LINE__, __FUNCTION__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...)                                                 \
-  fprintf(stderr,                                                        \
-          BRIGHT_YELLOW("[WARN] (f: %s, l: %d, fn: %s, e: %s) " M "\n"), \
+#define log_warn(M, ...)                                                  \
+  fprintf(stderr, BRIGHT_YELLOW("[WARN] (f:%s l:%d fn:%s e:%s) " M "\n"), \
           __FILE__, __LINE__, __FUNCTION__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...)                                                 \
-  fprintf(stderr, BRIGHT_GREEN("[INFO] (f: %s, l: %d, fn: %s) " M "\n"), \
-          __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define log_info(M, ...)                                                      \
+  fprintf(stderr, BRIGHT_GREEN("[INFO] (f:%s l:%d fn:%s) " M "\n"), __FILE__, \
+          __LINE__, __FUNCTION__, ##__VA_ARGS__)
 
 #define check(A, M, ...)       \
   if (!(A)) {                  \
