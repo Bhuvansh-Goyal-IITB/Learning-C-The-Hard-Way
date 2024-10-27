b333cc8933df5c9cb2472670300e64077b86214c
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-10-27 16:53:40 +0530

    added ex27

---
+++ ex27/main.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void copy(const char *from, char *to) {
+  int i = 0;
+  while ((to[i] = from[i]) != '\0') ++i;
+}
+
+int safecopy(int len_from, char *from, int len_to, char *to) {
+  assert(from != NULL && to != NULL && "from and to cant be null");
+  int max = len_from > len_to - 1 ? len_to - 1 : len_from;
+
+  if (len_from < 0 || len_to <= 0) {
+    return -1;
+  }
+
+  int i = 0;
+  for (; i < max; i++) {
+    to[i] = from[i];
+  }
+
+  to[i] = '\0';
+  return i;
+}
+
+int main() {
+  char str1[] = "Hello this is a string";
+  char *str2 = calloc(1, 23);
+
+  safecopy(23, str1, 23, str2);
+
+  printf("%s\n", str2);
+}
