1edc4e1b67e54c936cf50c9396f85cb07e186a5d
Bhuvansh Goyal <bhuvansh.iitb@gmail.com>
2025-06-07 12:49:11 +0000

    forgot to save one file

--- liblcthw/include/darray_algos.h
+++ liblcthw/include/darray_algos.h
@@ -3,8 +3,6 @@
 
 #include <darray.h>
 
-typedef int (*DArray_compare)(const void* a, const void* b);
-
 int DArray_qsort(DArray* array, DArray_compare cmp);
 // int DArray_headpsort(DArray* array, DArray_compare cmp);
 // int DArray_mergesort(DArray* array, DArray_compare cmp);
